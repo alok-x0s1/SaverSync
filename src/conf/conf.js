34d538d3e9a64b47621b30c54b8acb96a13c83ee
Alok Yadav <yalok6321@gmail.com>
2024-05-17 09:43:45 +0530

    npm i appwrite

---
+++ src/conf/conf.js
@@ -0,0 +1,10 @@
+const conf = {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectID: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseID: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionID: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID)
+}
+
+console.log(conf)
+
+export default conf
